6a9acbfda86e6b57d9a029d40fcb64178a7c9404
CristopherMartarello <crisrossi1313@gmail.com>
2025-06-03 16:43:25 -0300

    feat: add Home screen and fetching popular movies

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Text, View } from "react-native";
 
 const MovieDetails = () => {
-    return (
-        <View>
-            <Text>MovieDetails</Text>
-        </View>
-    );
-}
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  );
+};
 
 export default MovieDetails;
