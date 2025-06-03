cfdc888be38e2303131f0e88ea875bc296eae22b
CristopherMartarello <crisrossi1313@gmail.com>
2025-06-03 17:20:59 -0300

    feat: add value and onChangeText treatment to SearchBar component

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -3,18 +3,20 @@ import { Image, TextInput, View } from "react-native";
 
 interface Props {
   placeholder: string;
+  value?: string;
   onPress?: () => void;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({ placeholder, onPress }: Props) => {
+const SearchBar = ({ placeholder, value, onPress, onChangeText }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image source={icons.search} className="size-5" resizeMode="contain" />
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor={"#A8B5DB"}
         className="flex-1 ml-2 text-white"
       />
